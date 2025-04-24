263eca94e6bf76fbd6a79c64e9eeaaec99a2e19e
Karan gupta <gupta.karan1.gh@gmail.com>
2025-04-24 23:22:36 +0530

    background ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 class="text-9xl">
-    The Game Begins
-  </h1>
+    <div className="w-[100vw] h-[100vh] overflow-hidden ">
+      <Home/>
+    </div>
   )
 }
 
